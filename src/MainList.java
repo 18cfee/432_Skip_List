7f59a1b58eb61b782e3f64bd71f0bb0183992abb
Carl Fee <15cfee@gmail.com>
2017-10-22 17:51:34 -0600

    keep-this

---
+++ src/MainList.java
@@ -0,0 +1,42 @@
+public class MainList {
+    private static final int height = 4;
+    private static final Node headNodes = null;
+    private static int totalNumberNodes = 0;
+    public static void main(String args[]){
+
+    }
+
+    // h is hardcoded in this one
+    public static void insert(Node node, int h){
+
+    }
+
+    // h will be chosen automatically in this one
+    public static void insert(Node node){
+
+    }
+
+    //
+    public static boolean deleteNode(int value){
+        return false;
+    }
+
+    public static boolean searchNode(int value){
+        return false;
+    }
+
+    // Based on distribution
+    public static int pickRandomHeight(){
+        return 1; // Change this
+    }
+
+    // array of nodes from highest level to lowest
+    public static Node[] chooseOTGT(int origin){
+        return null;
+    }
+
+    // Choose the new value based on the absolute positioning
+    public static int chooseNewOriginValue(){
+        return 0;
+    }
+}
