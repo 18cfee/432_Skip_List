87ef95b237f535b02202f6a7618b1dc0c5ceef58
Carl Fee <15cfee@gmail.com>
2017-10-22 17:46:13 -0600

    added-node-and-main

--- src/MainList.java
+++ src/MainList.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
-
 public class MainList {
+    private static final int height = 5;
+    private static final Node headNodes = null;
+    public static void main(String args[]){
+
+    }
+
+    // h is hardcoded in this one
+    public static void insert(Node node, int h){
+
+    }
+
+    // h will be chosen automatically in this one
+    public static void insert(Node node){
+
+    }
+
+    //
+    public static boolean deleteNode(int value){
+        return false;
+    }
+
+    public static boolean searchNode(int value){
+        return false;
+    }
+
+    // Based on distribution
+    public static int pickRandomHeight(){
+        return 1; // Change this
+    }
+
+    // array of nodes from highest level to lowest
+    public static Node[] chooseOTGT(int origin){
+        return null;
+    }
+
+    // Choose the new value based on the absolute positioning
+    public static int chooseNewOriginValue(){
+        return 0;
+    }
 }
--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Node {
+    public int value = 0;
+    public String myinfo = null;
+    public Node next = null;
+    public Node prev = null;
+    public Node(int value, String myinfo){
+        this.value = value;
+        this.myinfo = myinfo;
+    }
 }
