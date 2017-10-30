e6cc46efce7854d3417a69d68496b32985e0ad45
Carl Fee <15cfee@gmail.com>
2017-10-29 23:30:16 -0600

    single-level

--- src/MainList.java
+++ src/MainList.java
@@ -1,14 +1,78 @@
+import java.util.HashSet;
+
 public class MainList {
-    private static int height = 4;
-    private static final Node headNodes = null;
+    private static Node HEAD = null;
     private static int totalNumberNodes = 0;
+    public static int maxHeight = 1;
     public static void main(String args[]){
+        // initialize as a circle to itself
+        Node originalHEAD = new Node(4);
+        HEAD = originalHEAD;
+        Node init = new Node(-4);
+        HEAD.next = init;
+        init.next = HEAD;
+
+        printList();
+        insert(new Node(7),1);
+        insert(new Node(6),1);
+        insert(new Node(5),1);
+        insert(new Node(9),1);
+        insert(new Node(9),1);
+        insert(new Node(70),1);
+        insert(new Node(34),1);
+        printList();
+
 
     }
 
     // h is hardcoded in this one
-    public static void insert(Node node, int h){
+    public static boolean insert(Node node, int h){
+        int insertVal = node.value;
+        Node current = HEAD;
+        // bounds checking, since next is checked from here on
+        if(insertVal == HEAD.value){
+            return true;
+        }
+        while(true){
+            int onVal = current.value;
+            Node nextN = current.next;
+            int nextVal = nextN.value;
+            if(nextVal == insertVal) return true;
+            // right interval (include circular logic)
+            if((onVal < insertVal && insertVal < nextVal) || (onVal > nextVal && (insertVal > onVal || insertVal < nextVal))){
+                if(current.down == null){
+                    current.next = node;
+                    node.next = nextN;
+                    for (int i = 2; i <= h; i++) {
+                        node.up = new Node(insertVal);
+                        node.up.down = node;
 
+                        if(i > maxHeight){
+                            HEAD = node.up;
+                            maxHeight = i;
+                        }
+                        node = node.up;
+                    }
+                    return false;
+                } else{
+                    current = current.down;
+                }
+            } else current = nextN;
+        }
+    }
+
+    // Print out the List
+    public static void printList(){
+        Node current = HEAD;
+        Node levelStart = HEAD;
+        do{
+            System.out.print(current.value);
+            while(current.next.value != levelStart.value){
+                current = current.next;
+                System.out.print(" ::-> " + current.value);
+            }
+            System.out.println();
+        }while(current.down != null);
     }
 
     // h will be chosen automatically in this one
--- src/Node.java
+++ src/Node.java
@@ -1,14 +1,11 @@
 public class Node {
     public int value = 0;
-    public String myinfo = null;
     public Node next = null;
-    public Node prev = null;
     public Node up = null;
     public Node down = null;
     public int skip = 1;
     public int index = -1;
-    public Node(int value, String myinfo){
+    public Node(int value){
         this.value = value;
-        this.myinfo = myinfo;
     }
 }
